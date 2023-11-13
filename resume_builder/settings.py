1b9b1ad91618f370852e6ff1bd025d1af6ee1463
Bhekumuzi <128836396+bhekumuzithabethe@users.noreply.github.com>
2023-11-13 17:22:09 +0200

    Update settings.py

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 #Configuring staticfiles (images,css,JavaScript)
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'core/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
